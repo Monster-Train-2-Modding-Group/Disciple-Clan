a602dc23ba07059397c2dbe2289a148078fee042
Jackson Levitt <jackel1020@gmail.com>
2026-02-05 19:48:55 -0500

    status fun times

--- DiscipleClan.Plugin/StatusEffects/StatusEffectHideUntilBossState.cs
+++ DiscipleClan.Plugin/StatusEffects/StatusEffectHideUntilBossState.cs
@@ -10,88 +10,34 @@ namespace DiscipleClan.Plugin.StatusEffects
     /// </summary>
     public class StatusEffectHideUntilBossState : StatusEffectState
     {
-        public const string StatusId = "hideuntilboss";
+        private SaveManager? _saveManager;
+        private CombatManager? _combatManager;
+
+        public override void OnStacksAdded(CharacterState character, int numStacksAdded, CharacterState.AddStatusEffectParams addStatusEffectParams, ICoreGameManagers coreGameManagers)
+        {
+            SaveManager saveManager = coreGameManagers.GetSaveManager();
+            CombatManager combatManager = coreGameManagers.GetCombatManager();
+            if (saveManager == null || coreGameManagers == null)
+                return;
+
+            _saveManager = saveManager;
+            _combatManager = combatManager;
+        }
 
         public override bool GetUnitIsTargetable(bool inCombat)
         {
             if (!inCombat)
                 return true;
 
-            CombatManager? combatManager = TryGetCombatManager();
-            if (combatManager == null)
+            if (_combatManager == null || _saveManager == null)
                 return true;
 
-            try
-            {
-                SaveManager? saveManager = GetSaveManager(combatManager);
-                if (saveManager == null) return true;
-
-                object? scenarioData = saveManager.GetType().GetMethod("GetCurrentScenarioData")?.Invoke(saveManager, null);
-                if (scenarioData == null) return true;
-
-                object? spawnPattern = scenarioData.GetType().GetMethod("GetSpawnPattern")?.Invoke(scenarioData, null);
-                if (spawnPattern == null) return true;
-
-                int numGroups = (int)(spawnPattern.GetType().GetMethod("GetNumGroups")?.Invoke(spawnPattern, null) ?? 0);
-                int turnCount = (int)(combatManager.GetType().GetMethod("GetTurnCount")?.Invoke(combatManager, null)
-                    ?? combatManager.GetType().GetProperty("TurnCount")?.GetValue(combatManager) ?? 0);
-
-                if (numGroups - turnCount > 0)
-                    return false;
-            }
-            catch
-            {
+            int turnCount = _combatManager.GetTurnCount();
+            if (turnCount > 0)
                 return true;
-            }
 
-            return true;
-        }
-
-        private static CombatManager? TryGetCombatManager()
-        {
-            try
-            {
-                // Try to get SaveManager from a known holder (e.g. PlayerManager), then CombatManager from SaveManager
-                Assembly? asm = typeof(CombatManager).Assembly;
-                Type? playerManagerType = asm.GetType("PlayerManager") ?? asm.GetType("TrainworksReloaded.Core.PlayerManager");
-                if (playerManagerType != null)
-                {
-                    PropertyInfo? instanceProp = playerManagerType.GetProperty("Instance", BindingFlags.Public | BindingFlags.Static);
-                    object? playerManager = instanceProp?.GetValue(null);
-                    if (playerManager != null)
-                    {
-                        SaveManager? saveManager = GetSaveManagerFromPlayer(playerManager);
-                        if (saveManager != null)
-                            return GetCombatManager(saveManager);
-                    }
-                }
-            }
-            catch { }
-            return null;
-        }
-
-        private static SaveManager? GetSaveManagerFromPlayer(object playerManager)
-        {
-            const BindingFlags f = BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public;
-            return playerManager.GetType().GetField("saveManager", f)?.GetValue(playerManager) as SaveManager;
-        }
-
-        private static SaveManager? GetSaveManager(CombatManager combatManager)
-        {
-            try
-            {
-                return combatManager.GetType().GetMethod("GetSaveManager")?.Invoke(combatManager, null) as SaveManager
-                    ?? combatManager.GetType().GetProperty("SaveManager")?.GetValue(combatManager) as SaveManager;
-            }
-            catch { }
-            return null;
-        }
-
-        private static CombatManager? GetCombatManager(SaveManager saveManager)
-        {
-            if (saveManager == null) return null;
-            const BindingFlags f = BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public;
-            return saveManager.GetType().GetField("combatManager", f)?.GetValue(saveManager) as CombatManager;
+            // Unit is not targetable if in combat and turn count is less than or equal to 0
+            return false;
         }
     }
 }
