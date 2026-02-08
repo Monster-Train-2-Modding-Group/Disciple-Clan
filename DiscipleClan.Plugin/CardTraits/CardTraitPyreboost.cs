0545d946fb93b2dde8af0cf941aca824c17a9412
Jackson Levitt <jackel1020@gmail.com>
2026-02-08 18:14:12 -0500

    made fireshaped more interesting, updated pyreboost and flashfire.

--- DiscipleClan.Plugin/CardTraits/CardTraitPyreboost.cs
+++ DiscipleClan.Plugin/CardTraits/CardTraitPyreboost.cs
@@ -19,7 +19,28 @@ namespace DiscipleClan.Plugin.CardTraits
 
             int pyreAttack = saveManager.GetDisplayedPyreAttack();
             int pyreNumAttacks = saveManager.GetDisplayedPyreNumAttacks();
+
+            Plugin.Logger.LogInfo($"Pyre attack: {pyreAttack}, Pyre num attacks: {pyreNumAttacks}, Param int: {this.GetParamInt()}");
             return damageParams.damage + pyreAttack * pyreNumAttacks * this.GetParamInt();
+        }
+
+
+        // Token: 0x06000CD8 RID: 3288 RVA: 0x00036B70 File Offset: 0x00034D70
+        public override string GetCardTooltipText()
+        {
+            return base.LocalizeTraitKey("DiscipleClan.Plugin_CardTraitPyreboost_TooltipText");
+        }
+
+        // Token: 0x06000CD9 RID: 3289 RVA: 0x00036B7D File Offset: 0x00034D7D
+        public override string GetCardTooltipId()
+        {
+            return base.LocalizeTraitKey("DiscipleClan.Plugin_CardTraitPyreboost");
+        }
+
+        // Token: 0x06000CDA RID: 3290 RVA: 0x00036B84 File Offset: 0x00034D84
+        public override string GetCardText()
+        {
+            return base.LocalizeTraitKey("DiscipleClan.Plugin_CardTraitPyreboost_CardText");
         }
     }
 }
